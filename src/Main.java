f07ad28bab83e9d1c71a945b556295d46ec83782
gentletempter <vitamin_bard@mail.ru>
2021-09-01 19:00:52 +0300

    Fixed logic

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ import registration.UserRegistration;
 public class Main {
     public static void main(String[] args) {
         DocumentVerifier.documentNumberVerification("5524-abc-4321-xyz-1a2b");
-        UserRegistration.register("Johny", "qwerty2", "qwerty2");
+        System.out.println(UserRegistration.register("John y", "qwerty2", "qwerty2"));
     }
 }
--- src/registration/UserRegistration.java
+++ src/registration/UserRegistration.java
@@ -4,11 +4,13 @@ package registration;
  * This class is used to verification user registration
  */
 public class UserRegistration {
-    public static void register(String login, String password, String confirmPassword) {
+    public static boolean register(String login, String password, String confirmPassword) {
         try {
-            System.out.println(registerUser(login, password, confirmPassword));
+            validator(login, password, confirmPassword);
+            return true;
         } catch (WrongLoginException | WrongPasswordException ex) {
             System.out.println(ex.getMessage());
+            return false;
         }
     }
 
@@ -16,25 +18,24 @@ public class UserRegistration {
      * @param login           <i>must be less than 20 characters and must not contain spaces.</i>
      * @param password        <i>must be less than 20 characters long, must not contains spaces and must contain at least one digit.</i>
      * @param confirmPassword <i>must match the password</i>
-     * @return {@code true} when the login and password are valid, else return {@code exception message}
+     * @return {@code true} when the login and password are valid, else return {@code false}
      * @throws WrongLoginException    when login are invalid
      * @throws WrongPasswordException when password are invalid or mismatch the confirmPassword
      */
-    private static boolean registerUser(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (login.contains(" ") || login.length() > 20) {
+    private static void validator(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (login.contains(" ") || login.length() >= 20) {
             throw new WrongLoginException("Invalid login!");
         }
         if (passwordVerifier(password, confirmPassword) != null) {
             throw new WrongPasswordException(passwordVerifier(password, confirmPassword));
         }
-        return true;
     }
 
     /**
      * Used for get detail message
      */
     private static String passwordVerifier(String password, String confirmPassword) {
-        if (password.contains(" ") || password.length() > 20 || !password.matches(".*\\d+.*")) {
+        if (password.contains(" ") || password.length() >= 20 || !password.matches(".*\\d+.*")) {
             return "Invalid password!";
         } else if (!password.equals(confirmPassword)) {
             return "Password mismatch!";
